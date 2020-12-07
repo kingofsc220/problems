b36fff78645592edc0a32ebfd32ec7982549592a
kingofsc220 <mailshence@163.com>
2020-12-07 17:14:34 +0800

    LeetCode problem 26   Remove Duplicates from Sorted Array

---
+++ src/Array/Pro26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,78 @@
+package Array;
+
+
+/**
+ * LeetCode problem 26   Remove Duplicates from Sorted Array
+ *
+ *
+ * Given a sorted array nums, remove the duplicates in-place
+ * such that each element appears only once and returns the new length.
+ *
+ * Do not allocate extra space for another array,
+ * you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ *
+ * Clarification:
+ *
+ * Confused why the returned value is an integer but your answer is an array?
+ *
+ * Note that the input array is passed in by reference,
+ * which means a modification to the input array will be known to the caller as well.
+ *
+ *
+ * Internally you can think of this:
+ *
+ * //nums is passed in by reference. (i.e., without making a copy)
+ * //any modification to nums in your function would be known by the caller.
+ * //using the length returned by your function, it prints the first len elements.
+ * int len = removeDuplicates(nums);
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2]
+ * Explanation: Your function should return length = 2,
+ *              with the first two elements of nums being 1 and 2 respectively.
+ *              It doesn't matter what you leave beyond the returned length.
+ *
+ *
+ * Example 2:
+ *
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4]
+ * Explanation: Your function should return length = 5,
+ *              with the first five elements of nums being modified to
+ *              0, 1, 2, 3, and 4 respectively.
+ *              It doesn't matter what values are set beyond the returned length.
+ *
+ *
+ * Constraints:
+ *      0 <= nums.length <= 3 * 104
+ *     -104 <= nums[i] <= 104
+ *      nums is sorted in ascending order.
+ */
+public class Pro26_RemoveDuplicatesFromSortedArray {
+
+    /**
+     * 一道非常典型的 排序指针问题
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int p1 = 0;
+        for (int p2 = 1; p2 < nums.length; p2++) {
+            if (nums[p1] < nums[p2]) {
+                nums[++p1] = nums[p2];
+            }
+        }
+
+        return p1 + 1;
+    }
+
+}
